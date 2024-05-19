a2b157663b357d16b9e7c919b7efbe00b91eeb28
AntonioaDn <antonioadn18@gmail.com>
2024-05-18 23:23:52 -0300

    javascript

---
+++ javascript/CFBcursos/aula002-modoEstrito/aula02.js
@@ -0,0 +1,6 @@
+"use strict" // Modo estrito -> Serve para obrigar o programador a fazer um codigo
+// mais legível.
+
+console.log("Esse é um dos meus primeiros programas em js.")
+let nome="Antônio"
+console.log("Nome: " + nome)
