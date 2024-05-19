0b2e66e3324c1c3b50f5e58fe2852f43f75a4afd
AntonioaDn <antonioadn18@gmail.com>
2024-05-18 23:23:33 -0300

    mistake

--- javascript/CFBcursos/aula001/aula01.js
+++
@@ -1,5 +0,0 @@
-// Linha de comentário
-/* Bloco de comentário */
-
-console.log("Olá mundo!")
-// Normalmente a tag <script src=""></script> e usada no final do body em um documento hmtl.
--- javascript/CFBcursos/aula002-modoEstrito/aula02.js
+++
@@ -1,6 +0,0 @@
-"use strict" // Modo estrito -> Serve para obrigar o programador a fazer um codigo
-// mais legível.
-
-console.log("Esse é um dos meus primeiros programas em js.")
-let nome="Antônio"
-console.log("Nome: " + nome)
