ff9842f5219e953f12679a6c8494d95801ebee25
AntonioaDn <antonioadn18@gmail.com>
2024-06-24 02:06:33 -0300

    javascript

---
+++ javascript/CFBcursos/aula058-objeto/aula58.js
@@ -0,0 +1,13 @@
+class Pessoa {
+    constructor(pnome) {
+        this.nome = pnome
+    }
+}
+
+let p1 = new Pessoa("Antônio")
+let p2 = new Pessoa("Breno")
+let p3 = new Pessoa("Lucas")
+
+console.log(p1.nome)
+console.log(p2.nome)
+console.log(p3.nome)
