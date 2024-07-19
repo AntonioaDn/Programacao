b34ccfbc5d83d175a627ddd0a9dd1604009fcc7f
AntonioaDn <antonioadn18@gmail.com>
2024-07-19 14:41:29 -0300

    calc numerico

--- Python/Estudos/1.Calculo-numerico/zeroDeFuncoes/main.py
+++ Python/Estudos/1.Calculo-numerico/zeroDeFuncoes/main.py
@@ -4,17 +4,23 @@ import numpy as np
 import pandas as pd
 import plotly.express as px
 
+
 # Funcao que será usada para calcular as raízes
 def funcao(x):
   return x**4 - 3*(x**3) + 2
   # x^4 - 3x^3 + 2
 
+# Area que sera criada a tabela.
+x = np.linspace(-10, 10, 400)
+y = funcao(x)
+
+tabela_bisseccao = pd.DataFrame(columns=['k', 'ak', 'bk', 'f(ak)', 'f(bk)', 'x k+1', 'f(x k+1)'])
+
+#
+
 # TVM (Teorema do Valor Medio)
 def TVM(x1, x2):
-  if funcao(x1)*funcao(x2) < 0:
-    return True
-  else:
-    return False
+    return funcao(x1) * funcao(x2) < 0
 
 # Teorema da bissecccao:
 def bissec(x1, x2, e):
@@ -22,23 +28,57 @@ def bissec(x1, x2, e):
     print("Não é possível afirmar que existe uma raiz nesse intervalo.")
     return
   else:
+    global tabela_bisseccao 
     k = 0
-    print(f"k | ak | bk | f(ak) | f(bk) | x k+1 | f(x k+1)")
     vm = (x1 + x2) / 2
+    nova_linha = pd.DataFrame({
+        'k': [k],
+        'ak': [x1],
+        'bk': [x2],
+        'f(ak)': [funcao(x1)],
+        'f(bk)': [funcao(x2)],
+        'x k+1': [vm],
+        'f(x k+1)': [funcao(vm)]
+    })
+    tabela_bisseccao = pd.concat([tabela_bisseccao, nova_linha], ignore_index=True)
+
     while abs(funcao(vm)) >= e:
       if TVM(x1, vm):
         x2 = vm
         vm = (x1 + x2) / 2
-        print(f"{k} | {x1} | {x2} | {funcao(x1)} | {funcao(x2)} | {vm} | {funcao(vm)}")
         k += 1
+        nova_linha = pd.DataFrame({
+        'k': [k],
+        'ak': [x1],
+        'bk': [x2],
+        'f(ak)': [funcao(x1)],
+        'f(bk)': [funcao(x2)],
+        'x k+1': [vm],
+        'f(x k+1)': [funcao(vm)]
+        })
+        tabela_bisseccao = pd.concat([tabela_bisseccao, nova_linha], ignore_index=True)
       else:
         x1 = vm
         vm = (x1 + x2) / 2
-        print(f"{k} | {x1} | {x2} | {funcao(x1)} | {funcao(x2)} | {vm} | {funcao(vm)}")
         k += 1
+        nova_linha = pd.DataFrame({
+        'k': [k],
+        'ak': [x1],
+        'bk': [x2],
+        'f(ak)': [funcao(x1)],
+        'f(bk)': [funcao(x2)],
+        'x k+1': [vm],
+        'f(x k+1)': [funcao(vm)]
+        })
+        tabela_bisseccao = pd.concat([tabela_bisseccao, nova_linha], ignore_index=True)
+
     return vm
+
+# Teorema da falsa posição:
+
 
 x1, x2 = map(float, input("Digite o intervalo [x1, x2]: ").split())
 e = float(input("Digite o criterio de parada: "))
 xz = bissec(x1, x2, e)
 print(f"O valor proximo ao criterio de parada eh: {xz}")
+print(tabela_bisseccao)
