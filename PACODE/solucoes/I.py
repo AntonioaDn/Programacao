5ec5c7f6a8b0e870d5c42aaf3c6f18471ec52497
AntonioaDn <antonioadn18@gmail.com>
2024-08-19 18:07:10 -0300

    pacode

--- PACODE/solucoes/I.py
+++ PACODE/solucoes/I.py
@@ -1,4 +1,17 @@
-a, b = map(int, input("").split())
-
-vetor = []
-
+n, k = map(int, input("").split())
+impares = pares = 0
+if n%2 == 0:
+    impares = pares = n/2
+else:
+    impares = (int(n/2)) + 1
+    pares = impares - 1
+if k <= impares:
+    resp = 1
+    for i in range(2, k+1):
+        resp += 2
+    print(resp)
+else:
+    resp = 2
+    for i in range(2, k-impares+1):
+        resp += 2
+    print(resp)
