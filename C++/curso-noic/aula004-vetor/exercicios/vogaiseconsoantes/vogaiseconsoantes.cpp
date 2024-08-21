4ba2128a737d472cf0b38e769682a3028b5e0485
Antônio Augusto <142694568+AntonioaDn@users.noreply.github.com>
2024-08-21 09:48:39 -0300

    noic

---
+++ C++/curso-noic/aula004-vetor/exercicios/vogaiseconsoantes/vogaiseconsoantes.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h> 
+
+using namespace std;
+
+int main () {
+    string nome, vogais="", consoantes="";
+    cin >> nome;
+    for (int i=0; i < nome.size(); i++) {
+        if (nome[i] == 'a' || nome[i] == 'e' || nome[i] == 'i' || nome[i] == 'o' || nome[i] == 'u') {
+            vogais += nome[i];
+        } else {
+            consoantes += nome[i];
+        }
+    }
+    cout << "Vogais: " << vogais << endl;
+    cout << "Consoantes: " << consoantes << endl;
+
+}
