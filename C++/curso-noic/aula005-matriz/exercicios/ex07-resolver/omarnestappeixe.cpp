b6ebaa4858d07341505a822b2f7d269527d56103
AntonioaDn <antonioadn18@gmail.com>
2024-08-22 22:24:36 -0300

    noic

--- C++/curso-noic/aula005-matriz/exercicios/ex07-resolver/omarnestappeixe.cpp
+++ C++/curso-noic/aula005-matriz/exercicios/ex07-resolver/omarnestappeixe.cpp
@@ -2,57 +2,25 @@
 
 using namespace std;
 
-int main () {
-    int n, m[100][100] = {0}, pos[2][2], xi, xf, yi, yf, maiorx, menorx;
-    cin >> n;
-    for (int i=0; i < 100; i++) {
-        for (int j=0; j < 100; j++) {
-            m[i][j] = 0;
-        }
-    }
+#define MAX 101
 
+
+int main () {
+    int matriz[MAX][MAX] = {0};
+    int n, xi, xf, yi, yf, area=0;
+    cin >> n;
     for (int i=0; i < n; i++) {
-        if (i==0) {
-            cin >> xi >> xf >> yi >> yf;
-            for (int z=xi-1; z <= xf-1; z++) {
-                m[yi-1][z] = 1;
-                m[yf-1][z] = 1;
+        cin >> xi >> xf >> yi >> yf;
+        for (int x=xi; x < xf; x++) {
+            for (int y=yi; y < yf; y++) {
+                matriz[x][y] = 1;
             }
-            maiorx = xf-1;
-            menorx = xi-1;
-        } else {
-            cin >> xi >> xf >> yi >> yf;
-            for (int z=xi-1; z <= xf-1; z++) {
-                m[yi-1][z] = 1;
-                m[yf-1][z] = 1;
-            }
-            if (maiorx < xf-1) maiorx = xf-1;
-            if (menorx > xi-1) menorx = xi-1;
         }
     }
-    int area = 0;
-    for (int i=menorx; i <= maiorx; i++) {
-        int inicio, final, indice=0;
-        while (m[indice][i] != 1) indice++;
-        inicio = indice + 1;
-        indice = inicio;
-        while (indice < 100){
-            if (m[indice][i] == 1) {
-                final = indice;
-            }
-            indice++;
+    for (int i=0; i < MAX-1; i++) {
+        for (int j=0; j < MAX-1; j++) {
+            if (matriz[i][j]) area++;
         }
-        area += final - inicio + 1;
     }
     cout << area << endl;
-    ///////////////////////////
-    cout << "[" << endl;
-    for (int i=0; i < 20; i++) {
-        for (int j=0; j < 20; j++) {
-            cout << m[i][j] << ", ";
-        }
-        cout << endl;
-    }
-    cout << "]";
-    ///////////////////////////
 }
