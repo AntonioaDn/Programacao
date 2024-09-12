63191139fbcd691d02a457f47d28982f6fa08bdc
AntonioaDn <antonioadn18@gmail.com>
2024-09-12 18:27:23 -0300

    neps

---
+++ C++/sites-competitivos/nepsacademy/potencias_simples/main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#include <cstdio> // Add this line
+
+using namespace std;
+
+int main () {
+    double x, y;
+    cin >> x >> y;
+    double z;
+    z = pow(x, y);
+    cout << fixed << setprecision(4) << z << endl;
+}
