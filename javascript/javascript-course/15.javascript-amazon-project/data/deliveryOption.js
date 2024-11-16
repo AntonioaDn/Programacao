c47780a64b909654083ee620ca5a16fe44fa4e25
Antônio Augusto <142694568+AntonioaDn@users.noreply.github.com>
2024-11-16 11:56:41 -0300

    15l - Create a function that reuse code to calculate delivery date.

--- javascript/javascript-course/15.javascript-amazon-project/data/deliveryOption.js
+++ javascript/javascript-course/15.javascript-amazon-project/data/deliveryOption.js
@@ -1,3 +1,5 @@
+import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
+
 export const deliveryOptions = [
   {
     id: '1', 
@@ -24,4 +26,10 @@ export function getDeliveryOption(deliveryOptionId) {
     }
   });
   return matchingDeliveryOption || deliveryOptions[0];
+}
+
+export function calculateDeliveryDate(deliveryOption) {
+  const today = dayjs();
+  const deliveryDate = today.add(deliveryOption.deliveryDays, 'day');
+  return deliveryDate.format('dddd, MMMM D');
 }
