17f5ccac582948b576cbda8aa3237b1ea1356c12
Antônio Augusto <142694568+AntonioaDn@users.noreply.github.com>
2024-11-16 10:27:24 -0300

    15g - Making payment summary more interactive.

--- javascript/javascript-course/15.javascript-amazon-project/scripts/checkout/paymentSummary.js
+++ javascript/javascript-course/15.javascript-amazon-project/scripts/checkout/paymentSummary.js
@@ -6,12 +6,14 @@ import { formatCurrency } from '../utils/money.js'
 export function renderPaymentSummary() {
   let productPriceCents = 0;
   let shippingPriceCents = 0;
+  let totalItems = 0;
   
   cart.forEach( (cartItem) => {
     const product = getProduct(cartItem.productId);
     productPriceCents += product.priceCents * cartItem.quantity;
     const deliveryOption = getDeliveryOption(cartItem.deliveryOptionId);
     shippingPriceCents += deliveryOption.priceCents;
+    totalItems += cartItem.quantity;
   });
 
   const totalBeforeTaxCents = productPriceCents + shippingPriceCents;
@@ -25,7 +27,7 @@ export function renderPaymentSummary() {
     </div>
 
     <div class="payment-summary-row">
-      <div>Items (3):</div>
+      <div>Items (${totalItems}):</div>
       <div class="payment-summary-money">
         $${formatCurrency(productPriceCents)}
       </div>
