42fca73d4b360d9407819ea6a7256d65209a9863
Antônio Augusto <142694568+AntonioaDn@users.noreply.github.com>
2024-11-19 17:46:53 -0300

    17c - Solution

--- javascript/javascript-course/17.object-oriented-programming/data/car.js
+++ javascript/javascript-course/17.object-oriented-programming/data/car.js
@@ -1,14 +1,32 @@
 class Car {
   brand;
   model;
+  speed;
 
   constructor(brand, model) {
     this.brand = brand;
     this.model = model;
+    this.speed = 0;
   }
 
   displayInfo() {
-    console.log(`${this.brand} ${this.model}`);
+    console.log(`${this.brand} ${this.model}, Speed: ${this.speed} km/h`);
+  }
+
+  go() {
+    if (this.speed + 5 > 200) {
+      this.speed = 200;
+    } else {
+      this.speed += 5;
+    }
+  }
+
+  brake() {
+    if (this.speed - 5 < 0) {
+      this.speed = 0;
+    } else {
+      this.speed -= 5;
+    }
   }
 }
 
@@ -16,5 +34,29 @@ const car1 = new Car('Toyota', 'Corolla');
 const car2 = new Car('Tesla', 'Model 3');
 console.log(car1);
 console.log(car2);
+
+car1.go();
+car1.go();
+car1.go();
+car1.go();
+car1.go();
+car2.go();
+car2.go();
+car2.go();
+car2.go();
+car2.go();
+car2.go();
+car2.go();
+car2.go();
+car2.go();
+car1.displayInfo();
+car2.displayInfo();
+car1.brake();
+car1.brake();
+car1.brake();
+car1.brake();
+car1.brake();
+car1.brake();
+car1.brake();
 car1.displayInfo();
 car2.displayInfo();
