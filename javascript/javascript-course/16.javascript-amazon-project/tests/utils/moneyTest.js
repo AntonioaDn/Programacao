ea6e303fe2db4e0653c6ae6fc7671e972b00ef96
Antônio Augusto <142694568+AntonioaDn@users.noreply.github.com>
2024-11-19 12:04:51 -0300

    just ignore

--- javascript/javascript-course/16.javascript-amazon-project/tests/utils/moneyTest.js
+++ javascript/javascript-course/16.javascript-amazon-project/tests/utils/moneyTest.js
@@ -12,4 +12,12 @@ describe('test suite: formatCurrency', () => {
   it('rounds up to the nearest cent', () => {
     expect(formatCurrency(2000.5)).toEqual('20.01');
   });
+
+  it('Tests 2000.4 if is rounded down for the neares cent', () => {
+    expect(formatCurrency(2000.4)).toEqual('20.00');
+  });
+
+  it('works with negative numbers', () => {
+    expect(formatCurrency(-100)).toEqual('-1.00')
+  });
 });
